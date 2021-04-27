5b8489239af103fcad13011a36beb0774e53b384
Shivam28nov <76863134+Shivam28nov@users.noreply.github.com>
2021-04-27 20:45:10 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,34 @@
+class Paper{
+	constructor(x,y,r){
+
+	// assign options to the paper
+     var options={
+
+         isStatic:false,
+		 restitution:0.3,
+		 friction:0,
+		 density:1.2
+	 }
+
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		this.image = loadImage("paper.png");
+		World.add(world, this.body);
+
+	}
+
+	display(){
+
+			var paperpos=this.body.position;		
+			push()
+			translate(paperpos.x, paperpos.y);
+			imageMode(RADIUS);
+			//draw the ellipse here to display the rubber ball
+            image(this.image,0,0,this.r,this.r);
+			
+			pop()
+	}
+
+}
---
+++ sketch.js
@@ -0,0 +1,55 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var dustbinObj,groundObject;
+var world;
+var paper,paperImg;
+
+
+function setup() {
+	
+	// to create canvas
+	createCanvas(1300, 700);
+
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1110,650);
+    paper = new Paper(100,500,90);
+	Engine.run(engine);
+	
+}
+
+
+function draw() {
+
+  rectMode(CENTER);
+  background(230);
+
+  groundObject.display();
+  dustbinObj.display();
+  paper.display();
+ 
+
+} 
+
+function keyPressed(){
+
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:178,y:-173});
+
+	}
+}
+
+
+	
+
+
+
